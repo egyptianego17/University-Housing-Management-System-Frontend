0e0166029645d64525a3793742f42a483c9ac2c4
egyptianego17 <abdulrahman.omar17h@gmail.com>
2024-11-06 23:02:21 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,37 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
-import manifest from './manifest.json';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      manifest,
+      manifest: {
+        theme_color: "#2473c6",
+        background_color: "#1f3560",
+        icons: [
+          {
+            purpose: "maskable",
+            sizes: "512x512",
+            src: "icon512_maskable.png",
+            type: "image/png"
+          },
+          {
+            purpose: "any",
+            sizes: "512x512",
+            src: "icon512_rounded.png",
+            type: "image/png"
+          }
+        ],
+        orientation: "portrait", 
+        display: "standalone",
+        dir: "rtl",
+        lang: "en-US",
+        name: "Nezam",
+        short_name: "Nezam",
+        start_url: "/"
+      },
       includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-      // switch to "true" to enable sw on development
       devOptions: {
         enabled: false,
       },
